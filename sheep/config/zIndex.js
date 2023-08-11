c7ef4fc3339199d700bb74aaa60881ab0c328381
jtyin <jtyin@depingroup.com>
2023-08-11 15:44:06 +0800

    添加 sheep/config

---
+++ sheep/config/zIndex.js
@@ -0,0 +1,11 @@
+export default {
+  toast: 10090,
+  noNetwork: 10080,
+  popup: 10075, // popup包含popup，actionsheet，keyboard，picker的值
+  mask: 10070,
+  navbar: 980,
+  topTips: 975,
+  sticky: 970,
+  indexListSticky: 965,
+  popover: 960,
+};
