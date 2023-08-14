040db9c9972e8ae1bb3b3ae484916167bad70e5c
jtyin <jtyin@depingroup.com>
2023-08-14 13:06:34 +0800

    platform 平台

--- sheep/platform/provider/wechat/index.js
+++ sheep/platform/provider/wechat/index.js
@@ -1,1 +1,15 @@
-import {} from 'lodash'
+// #ifdef H5
+import service from './officialAccount';
+// #endif
+
+// #ifdef MP-WEIXIN
+import service from './miniProgram';
+// #endif
+
+// #ifdef APP-PLUS
+import service from './openPlatform';
+// #endif
+
+const wechat = service;
+
+export default wechat;
